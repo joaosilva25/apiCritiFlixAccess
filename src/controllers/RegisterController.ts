da8f03d76336b75df18d09a4b09c2fb6e681c1dc
JoaoSilva25 <joaosilva2505@outlook.com>
2024-04-10 16:13:22 -0300

    fix register

--- src/controllers/RegisterController.ts
+++ src/controllers/RegisterController.ts
@@ -7,18 +7,18 @@ export const createNewUser=async(req:Request,res:Response)=> {
 
     const {email,userName,password}=req.body
 
-    let strongPassword=validator.isStrongPassword(password,{minLength:6,minUppercase:1,minSymbols:1})
+    if(userName && email && password) {
 
-    if(validator.isEmail(email) && strongPassword) {
+        let strongPassword=validator.isStrongPassword(password,{minLength:6,minUppercase:1,minSymbols:1})
 
-        if(userName && email && password) {
+        if (validator.isEmail(email) && strongPassword) {
             await registerUser(res,userName,email,password)
         }
         else {
-            res.json({message:"Preencha os campos para prosseguir"})
+            res.json({message:"Escolha uma senha forte que atenda aos seguintes critérios mínimos: tenha no mínimo 6 caracteres, inclua pelo menos 1 letra maiúscula e 1 simbolo"})
         }
     }
     else {
-        res.json({message:"Escolha uma senha forte que atenda aos seguintes critérios mínimos: tenha no mínimo 6 caracteres, inclua pelo menos 1 letra maiúscula e 1 simbolo"})
+        res.json({message:"Preencha os campos para prosseguir"})
     }
 }
